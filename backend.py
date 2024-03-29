bc147ccda194fd107f980e62313c062e7d7f254e
spiderberry <159087579+spiderberry@users.noreply.github.com>
2024-03-28 23:21:30 -0400

    races work
    add 100m function and 100m race functions.
    
    all database functions work. can communicate properly with db.
    
    function for populating races is outside of race function. should try putting in race function
    
    need to add create player function

--- backend.py
+++ backend.py
@@ -1,5 +1,13 @@
 import sqlite3
 from sqlite3 import Error
+from collections import namedtuple
+
+Athlete = namedtuple('Athlete', [
+    'id', 'first_name', 'last_name', 'age', 'acceleration', 'endurance', 
+    'form', 'mental', 'speed', 'stamina', 'start', 'place', 'last_race_time', 
+    'last_race_type', 'fastest_100m', 'fastest_200m', 'fastest_400m', 
+    'races_ran', 'overall', 'overall_100m', 'overall_200m', 'overall_400m'
+])
 
 def add_athlete(first_name, last_name, age, acceleration, endurance, form, mental, speed, stamina, start):
 
@@ -8,10 +16,9 @@ def add_athlete(first_name, last_name, age, acceleration, endurance, form, menta
         cursor = connection.cursor()
         cursor.execute(
             "INSERT INTO Athletes(first_name, last_name, age, acceleration, endurance, form, mental, speed, start)"
-            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (first_name, last_name, age, acceleration, endurance, form, mental, speed, stamina, start)
+            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (first_name, last_name, age, acceleration, endurance, form, mental, speed, stamina, start,)
         )
         connection.commit()
-        connection.close()
         
     except Error as e:
         print(e)
@@ -28,9 +35,8 @@ def view_athletes():
         cursor.execute(
             "SELECT * FROM Athletes"
         )
-        rows = cursor.fetchall()
-        connection.close()
-        return rows
+        athletes = [Athlete(*athlete) for athlete in cursor.fetchall()]
+        return athletes
 
     except Error as e:
         print(e)
@@ -39,18 +45,20 @@ def view_athletes():
         if connection:
             connection.close()
 
-def search_athlete(id="", first_name="", last_name="", age="", acceleration="", endurance="", form="", mental="", speed="", stamina="", start="", overall="", overall_100m="", overall_200m="", overall_400m=""):
+def search_athlete(id="", first_name="", last_name="", age="", acceleration="", endurance="", form="", mental="", speed="", stamina="", start="", overall="", overall_100m="", overall_200m="", overall_400m="", place="", last_race_time="", last_race_type="", fastest_100m="", fastest_200m="", fastest_400m="", races_ran=""):
 
     try:
         connection = sqlite3.connect(r"C:/Users/write/OneDrive/Documents/dk/tracksim/track-simulator/track.db")
         cursor = connection.cursor()
         cursor.execute(
-            "SELECT * FROM Athletes WHERE id=? OR first_name=? OR last_name=? OR age=? OR acceleration=? OR endurance=? OR form=? OR mental=? OR speed=? OR stamina=? OR start=? OR overall=? OR overall_100m=? OR overall_200m=? OR overall_400m=?"
-            , (id, first_name, last_name, age, acceleration, endurance, form, mental, speed, stamina, start, overall, overall_100m, overall_200m, overall_400m)
+            "SELECT * FROM Athletes WHERE id=? OR first_name=? OR last_name=? OR age=? OR acceleration=? OR endurance=? OR form=? OR mental=? OR speed=? OR stamina=? OR start=? OR overall=? OR overall_100m=? OR overall_200m=? OR overall_400m=? OR place=? OR last_race_time=? OR last_race_type=? OR fastest_100m=? OR fastest_200m=? OR fastest_400m=? OR races_ran=?"
+            , (id, first_name, last_name, age, acceleration, endurance, form, mental, speed, stamina, start, overall, overall_100m, overall_200m, overall_400m, place, last_race_time, last_race_type, fastest_100m, fastest_200m, fastest_400m, races_ran,)
         )
-        rows = cursor.fetchall()
-        connection.close()
-        return rows
+        athletes = cursor.fetchall()
+
+        if athletes:
+            athletes = [Athlete(*athlete) for athlete in athletes]
+            return athletes
     
     except Error as e:
         print(e)
@@ -58,7 +66,7 @@ def search_athlete(id="", first_name="", last_name="", age="", acceleration="", 
     finally:
         if connection:
             connection.close()
-#Unused
+
 def get_athlete(id):
 
     try:
@@ -66,11 +74,12 @@ def get_athlete(id):
         cursor = connection.cursor()
         cursor.execute(
             "SELECT * FROM Athletes WHERE id=?"
-            , (id)
+            , (id,)
         )
         athlete_data = cursor.fetchone()
-        connection.close()
-        return athlete_data
+
+        if athlete_data:
+            return Athlete(*athlete_data)
     
     except Error as e:
         print(e)
@@ -79,17 +88,34 @@ def get_athlete(id):
         if connection:
             connection.close()
 
-def update_athlete(id, first_name="", last_name="", age="", acceleration="", endurance="", form="", mental="", speed="", stamina="", start=""):
-
+def update_athlete(id, first_name="", last_name="", age="", acceleration="", endurance="", form="", mental="", speed="", stamina="", start="", place="", last_race_time="", last_race_type="", fastest_100m="", fastest_200m="", fastest_400m="", races_ran=""):
     try:
         connection = sqlite3.connect(r"C:/Users/write/OneDrive/Documents/dk/tracksim/track-simulator/track.db")
         cursor = connection.cursor()
-        cursor.execute(
-            "UPDATE Athletes SET first_name=?, last_name=?, age=?, acceleration=?, endurance=?, form=?, mental=?, speed=?, stamina=?, start=? WHERE id=?",
-            (first_name, last_name, age, acceleration, endurance, form, mental, speed, stamina, start, id)
-        )
-        connection.commit()
-        connection.close()
+        
+        # Check if the athlete with the given id exists
+        cursor.execute("SELECT id FROM Athletes WHERE id=?", (id,))
+        existing_id = cursor.fetchone()
+        
+        if existing_id:
+            # Construct the SET clause dynamically based on provided parameters
+            params = []
+            set_clause_parts = []
+            for attr, value in [("first_name", first_name), ("last_name", last_name), ("age", age), ("acceleration", acceleration), ("endurance", endurance), ("form", form), ("mental", mental), ("speed", speed), ("stamina", stamina), ("start", start), ("place", place), ("last_race_time", last_race_time), ("last_race_type", last_race_type), ("fastest_100m", fastest_100m), ("fastest_200m", fastest_200m), ("fastest_400m", fastest_400m), ("races_ran", races_ran)]:
+                if value != "":
+                    set_clause_parts.append(f"{attr}=?")
+                    params.append(value)
+            set_clause = ", ".join(set_clause_parts)
+            
+            # Append the id parameter to the end
+            params.append(id)
+            sql_statement = f"UPDATE Athletes SET {set_clause} WHERE id=?"
+            
+            # Execute the SQL statement with the provided values
+            cursor.execute(sql_statement, params)
+            connection.commit()
+        else:
+            print(f"Athlete with id {id} not found.")
 
     except Error as e:
         print(e)
@@ -97,5 +123,3 @@ def update_athlete(id, first_name="", last_name="", age="", acceleration="", end
     finally:
         if connection:
             connection.close()
-
-#print(get_athlete("2"))
--- track_db.py
+++ track_db.py
@@ -1,38 +1,109 @@
 import backend
-import sqlite3
-
-def get_athlete_info(athlete_id):
-    connection = sqlite3.connect("your_database.db")
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM athletes WHERE id=?", (athlete_id,))
-    athlete_data = cursor.fetchone()
-    connection.close()
-    return athlete_data
+import random
+import numpy as np
+from collections import namedtuple
 
 def format_athlete_info(athlete_data):
-    athlete_data = athlete_data[0]
-    if athlete_data:
-        athlete_info = (
-            f"Name: {athlete_data[1]} {athlete_data[2]}, Age: {athlete_data[3]}, "
-            f"Acceleration: {athlete_data[4]}, Endurance: {athlete_data[5]}, "
-            f"Form: {athlete_data[6]}, Mental: {athlete_data[7]}, "
-            f"Speed: {athlete_data[8]}, Stamina: {athlete_data[9]}, "
-            f"Start: {athlete_data[10]}, Overall: {athlete_data[18]}, "
-            f"100m Overall: {athlete_data[19]}, 200m Overall: {athlete_data[19]}, "
-            f"400m Overall: {athlete_data[20]}, Races ran: {athlete_data[17]}, "
-            f"Place in Last Race: {athlete_data[11]}, "
-            f"Last Race Type: {athlete_data[12]}, Last Race Time: {athlete_data[13]}, "
-            f"Fastest 100m: {athlete_data[14]}, Fastest 200m: {athlete_data[15]}, "
-            f"Fastest 400m: {athlete_data[16]}"
-        )
-        return athlete_info
-    else:
+    if athlete_data is None:
         return "Athlete not found."
 
-athlete_id = "1"  # Change this to the ID of the athlete you want to retrieve info for
-athlete_data = backend.search_athlete(athlete_id)
-athlete_info_str = format_athlete_info(athlete_data)
-print(athlete_info_str)
+    (athlete_id, first_name, last_name, age, acceleration, endurance, form, mental, 
+     speed, stamina, start, place, last_race_time, last_race_type, fastest_100m, 
+     fastest_200m, fastest_400m, races_ran, overall, overall_100m, overall_200m, 
+     overall_400m) = athlete_data
+
+    return (
+        f"Name: {first_name} {last_name}, Age: {age}, "
+        f"Acceleration: {acceleration}, Endurance: {endurance}, "
+        f"Form: {form}, Mental: {mental}, Speed: {speed}, "
+        f"Stamina: {stamina}, Start: {start}, Overall: {overall}, "
+        f"100m Overall: {overall_100m}, 200m Overall: {overall_200m}, "
+        f"400m Overall: {overall_400m}, Races ran: {races_ran}, "
+        f"Place in Last Race: {place}, Last Race Type: {last_race_type}, "
+        f"Last Race Time: {last_race_time}, Fastest 100m: {fastest_100m}, "
+        f"Fastest 200m: {fastest_200m}, Fastest 400m: {fastest_400m}"
+    )
+
+    
+def sort_race(runners_time, runners):
+
+    runners_time.sort()
+
+    for i in runners:
+        for j in range(len(runners_time)):
+            
+            if i.last_race_time == runners_time[j]:
+                i.place = j + 1
+
+def one_hundred_meters(runner):
+    
+    runner
+    mean100m = 10.7459527863
+    sd100m = 0.133776557
+    max_time = 8.00  # Maximum overall performance time
+    overall = runner[19]
+
+    # Adjusting mean and standard deviation based on overall performance
+    mean = mean100m - (overall - 50) * (mean100m - max_time) / 50
+    
+    # Generating random time within the adjusted mean and standard deviation
+    time = np.random.normal(mean, sd100m)
+    
+    return time
+
+def one_hundred_meters_race(runners):
+        
+        runners_time = []
+        winning_time = 15.00
+        winner = None
+
+        #Simulate race and find winner
+        for runner in runners:
+            runner_time = float(f"{one_hundred_meters(runner):.2f}")
+            runners_time.append(runner_time)
+
+            if runner_time < winning_time:
+                winning_time = runner_time
+                winner = f"{runner.first_name} {runner.last_name}"
+        
+        #Update the database records
+        for runner, time in zip(runners, runners_time):
+            backend.update_athlete(runner.id, last_race_time=time, last_race_type="100m")
+
+            if time < runner.fastest_100m:
+                backend.update_athlete(runner.id, fastest_100m=time)
+
+            backend.update_athlete(runner.id, stamina=max(runner.stamina-10, 0))
+            backend.update_athlete(runner.id, races_ran=(runner.races_ran+1))
+        
+        #Print the race results
+        print(f"100m: {len(runners)} runners")
+        print("------------------------------")
+        for i, runner in enumerate(runners):
+            print(f"Lane {i+1}: {runner.first_name} {runner.last_name} ran {runners_time[i]}. OVR: {runner.overall_100m}")
+
+        print(f"The winner is {winner} with a time of {winning_time}")
+
+        # Sort the race results if needed
+        sort_race(runners_time, runners)
+        
+chosen_numbers = set()
+runners = []
+
+for i in range(8):
+    while True:
+        random_number = str(random.randint(1,99))
+        if random_number not in chosen_numbers:
+            chosen_numbers.add(random_number)
+            break
+
+    athlete = backend.get_athlete(random_number)
+    print(athlete)
+    runners.append(athlete)
+runners2 = [backend.get_athlete(94), backend.get_athlete(2), backend.get_athlete(49), backend.get_athlete(58), backend.get_athlete(99), backend.get_athlete(7), backend.get_athlete(28), backend.get_athlete(40)]
+print("Race time!")
+print(runners)
+one_hundred_meters_race(runners)
 
 def game_loop():
     pass
